2525f7748c6a830a041725aee6046f9aaf0298ea
Arjay <arjay.reyes@sjsu.edu>
2024-12-05 15:41:02 -0800

    Final changes

--- test-files/selenium-test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-# Start the browser
-driver = webdriver.Chrome()  # Or use `webdriver.Firefox()` for GeckoDriver
-driver.get("https://example.com")
-
-# Close the browser
-driver.quit()
